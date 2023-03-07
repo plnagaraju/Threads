76801ff977932cc6c9a6bcf5f756b8380b919036
Lakshmi Nagaraju, <plnr206@gmail.com>
2023-03-07 13:33:39 +0530

    Threads creation using Runnable interface

--- src/demo2/App.java
+++ src/demo2/App.java
@@ -1,7 +1,44 @@
 package demo2;
 
+class Runner implements Runnable {
+
+	
+	public void run() {
+		
+		for(int i=0; i<10; i++) {
+			System.out.println("Hello- "+i);
+		}
+		
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+	
+	
+	
+	
+}
 
 public class App {
+	
+	public static void main(String[] args) {
+		
+		Thread thread1 = new Thread(new Runner());
+		
+		thread1.start();
+		
+		Thread thread2 = new Thread(new Runner());
+		
+		thread2.start();
+		
+		
+	}
 	
 
 }
